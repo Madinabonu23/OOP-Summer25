4ece354a8a9ef20d5ed1529cf4859b11613c1a5d
Madinabonu23 <madinabonu674@gmail.com>
2025-03-13 12:07:19 +0000

    Add student details variables

---
+++ student_details.py
@@ -0,0 +1,17 @@
+# student_details.py
+
+#Variables for student data 
+first_name = "Madinabonu"
+last_name = "Jaloldinova"
+index_number = "35485"
+nationality = "Uzbek"
+starting_date = "13-10-2024"
+courses = ["Math","Basics of digital technology", "Basics of Marketing","Basics of telecommunications", "English ours II", "Ethics", "Logistics", "Object-oriented programming"]
+
+# Print student details 
+print("Student Details:")
+print(f"Name: {first_name} {last_name}")
+print(f"Index Number: { index_number}")
+print(f"Nationality: {nationality}")
+print(f"Starting Date: {starting_date}")
+print(f"Courses: {','.join(courses)}")
