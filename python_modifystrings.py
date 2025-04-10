a0997190a3266a2bd46556ee6c8d3d04ca9c2e07
Madinabonu23 <madinabonu674@gmail.com>
2025-04-10 11:15:20 +0000

    Add new file

---
+++ python_modifystrings.py
@@ -0,0 +1,9 @@
+# creat a modify string 
+
+a = "Germany and Poland!"
+
+print(a.upper())
+print(a.lower())
+print(a.strip()) # returns "Germany and Poland!"
+print(a.replace("M", "B"))
+print(a.split(",")) # returns ['Germany' and 'Poland'!]
