3cf46ddea1f5ce37c5e4b61de643eea0511b93b4
Madinabonu23 <madinabonu674@gmail.com>
2025-04-24 10:16:08 +0000

    Add new file

---
+++ w3_school/string.concatenation.py
@@ -0,0 +1,14 @@
+# String Cobcatenation 
+
+#Merge variable a with variable b into variable c
+a= "Apple"
+b= "Kiwi"
+c= a + b 
+print(c)
+
+
+#To add a space between them, add a " " 
+a= "Apple"
+b= "Kiwi"
+c= a+ " "+ b 
+print(c)
