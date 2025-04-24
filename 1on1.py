6a7abbd22eeaefb81cd2f34e419d11b48ed803f0
Madinabonu23 <madinabonu674@gmail.com>
2025-04-24 11:23:02 +0000

    1on1

---
+++ 1on1.py
@@ -0,0 +1,14 @@
+print("Madinabonu Jaloldinova")
+
+x = 23 
+y = "Madina"
+print(x)
+print(y)
+
+fruits = ["Kiwi","Apple","Strawberry"]
+print(fruits)
+for fruit in fruits: 
+    print(fruit) 
+
+
+
---
+++ w3_school/format.strings.py
@@ -0,0 +1,4 @@
+# Format Strings 
+
+age = 23 
+txt = "My name is Madina , I am " + age 
